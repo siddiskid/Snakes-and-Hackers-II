8d92cf469949ec434d99f992b28af7081c10586f
Siddarth <siddarth_ch@outlook.com>
2022-08-13 22:21:15 +0530

    incorporated tictactoe into the website

--- homepage/script.js
+++
@@ -1,9 +0,0 @@
-const abab=document.getElementById('initial')
-const islands = document.getElementById('islands')
-const logo = document.getElementById('logotop')
-
-function showislands(){
-    abab.hidden = true
-    islands.style.display = 'flex'
-    logo.style.display = 'block'
-}
