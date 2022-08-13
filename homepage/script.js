a77d5b348cbd892900cf22acdfb86b3612cd594d
SiddIsKid <85704038+SiddIsKid@users.noreply.github.com>
2022-08-13 16:40:07 +0530

    uploaded homepage files

---
+++ homepage/script.js
@@ -0,0 +1,9 @@
+const abab=document.getElementById('initial')
+const islands = document.getElementById('islands')
+const logo = document.getElementById('logotop')
+
+function showislands(){
+    abab.hidden = true
+    islands.style.display = 'flex'
+    logo.style.display = 'block'
+}
